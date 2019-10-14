492c614f91d942e45b9d2f0c07e7a4ea592441b8
Juan Perdomo <jcperdomoq@uqvirtual.edu.co>
2019-10-14 14:40:00 -0500

    shopping order

---
+++ resources/views/emails/message/productRequest.blade.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Solicitud</title>
+
+    <style>
+        .contenido{
+            display: flex;
+        }
+
+        body{
+            font-family: "Arial", serif;
+            margin: 0;
+        }
+
+        .imagen{
+            width: 50%;
+        }
+        .content-text{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            text-align: center;
+            margin-right: 3%;
+        }
+
+        .titulo{
+            text-align: left;
+            font-style: italic;
+            font-weight: bold;
+            font-size: 40px;
+            margin-bottom: 10%;
+            color: #27D8DB;
+        }
+
+        .texto{
+            text-align: center;
+            color: #A3A3A3;
+        }
+        .texto-left{
+            text-align: left;
+            color: #A3A3A3;
+        }
+        .texto-rigth{
+            text-align: right;
+            color: #27D8DB;
+        }
+        .footer{
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            text-align: center;
+            padding: 5px;
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+        }
+        @media (max-width: 550px){
+            .imagen{
+                display: none;
+            }
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="contenido">
+        <img src="{{asset('img/solicitud/Group3.svg')}}" class="imagen"/>
+        <div class="content-text">
+            <h1 class="titulo">¡Hola {{$name}}!</h1>
+            <p class="texto-left">
+                Solicitud de producto:
+            </p>
+            <p class="texto">
+                Gracias por tu sugerencia, estamos trabajando para satisfacer las necesidades de cada uno de nuestros clientes,
+                revisaremos los productos que deseas encontrar en nuestra tienda y te avisaremos cuando est&eacute;n disponibles.
+            </p>
+            <p class="texto">
+                Con cariño,
+            </p>
+            <p class="texto">
+                Feel Fit
+            </p>
+            <p class="texto-rigth">
+                Cuidarte, se siente bien.
+            </p>
+        </div>
+    </div>
+    <div class="footer">
+        <img src="{{asset('img/welcome/logo.svg')}}"/>
+    </div>
+</div>
+</body>
+</html>
--- resources/views/emails/shopping/order.blade.php
+++ resources/views/emails/shopping/order.blade.php
@@ -5,46 +5,20 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Solicitud</title>
 
     <style>
-
-        .caja{
-            border:2px red solid;
-        }
-
-        .contenido{
-            display: flex;
-        }
-
         body{
             font-family: "Arial", serif;
-            margin: 0;
         }
-
-        .imagen{
-            width: 50%;
-        }
-        .content-text{
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            text-align: center;
-            margin-right: 3%;
-        }
-
         .titulo{
             text-align: left;
             font-style: italic;
             font-weight: bold;
             font-size: 40px;
-            margin-bottom: 10%;
+            margin-bottom: 5%;
             color: #27D8DB;
-        }
-
-        .texto{
-            text-align: center;
-            color: #A3A3A3;
         }
         .texto-left{
             text-align: left;
@@ -54,7 +28,7 @@
             text-align: right;
             color: #27D8DB;
         }
-        .footer{
+        .pie-pagina{
             position: absolute;
             bottom: 0;
             width: 100%;
@@ -62,40 +36,48 @@
             padding: 5px;
             box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
         }
-        @media (max-width: 550px){
-            .imagen{
-                display: none;
-            }
-        }
     </style>
 </head>
 <body>
-<div class="container">
-    <div class="contenido">
-        <img src="{{asset('img/solicitud/Group3.svg')}}" class="imagen"/>
-        <div class="content-text">
-            <h1 class="titulo">¡Hola {{$shopping->user->name}}!</h1>
-            <p class="texto-left">
-                Solicitud de producto:
-            </p>
-            <p class="texto">
-                Gracias por tu sugerencia, estamos trabajando para satisfacer las necesidades de cada uno de nuestros clientes,
-                revisaremos los productos que deseas encontrar en nuestra tienda y te avisaremos cuando est&eacute;n disponibles.
-            </p>
-            <p class="texto">
-                Con cariño,
-            </p>
-            <p class="texto">
-                Feel Fit
-            </p>
-            <p class="texto-rigth">
-                Cuidarte, se siente bien.
-            </p>
-        </div>
+<div class="container mt-5">
+    <div class="row d-flex flex-column">
+        <h1 class="titulo">¡Hola {{$shopping->user->name}}!</h1>
+        <p class="texto-left">
+            Gracias por comprar en FeelFit. Esperamos que hayas disfrutado de tu experiencia de compra con nostros.<br>
+        </p>
     </div>
-    <div class="footer">
-        <img src="{{asset('img/welcome/logo.svg')}}"/>
+    <div class="row mt-5">
+        <table class="table table-bordered mx-5">
+            <thead class="thead-dark text-center">
+            <tr>
+                <th scope="col">Nombre</th>
+                <th scope="col">Cantidad</th>
+                <th scope="col">Valor</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach ($shopping->details as $detail)
+            <tr>
+                <td>{{$detail->name}}</td>
+                <td class="text-right">{{$detail->quantity}}</td>
+                <td class="text-right font-italic">{{$detail->value}}</td>
+            </tr>
+            @endforeach
+            <tr>
+                <td colspan="2" class="font-weight-bold">Total</td>
+                <td class="text-right font-weight-bold font-italic">$ 2000</td>
+            </tr>
+            </tbody>
+        </table>
     </div>
+    <div class="row justify-content-end">
+        <p class="texto-rigth m-5">
+            Cuidarte, se siente bien.
+        </p>
+    </div>
+</div>
+<div class="pie-pagina">
+    <img src="{{asset('img/welcome/logo.svg')}}"/>
 </div>
 </body>
 </html>
