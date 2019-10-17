bca14c89980d3759970dc79e2446b3da856c55d6
mauricio suarez <mauroziux@gmail.com>
2019-10-17 00:41:30 -0500

    fix issue

--- app/Http/Controllers/Api/Products/ProductController.php
+++ app/Http/Controllers/Api/Products/ProductController.php
@@ -23,7 +23,7 @@ class ProductController extends ApiController
      */
     public function index()
     {
-        $data = Product::where('quantity','>',0)->get();
+        $data = Product::all();
 
         return $this->showAll($data, 200, ProductCollection::class);
     }
