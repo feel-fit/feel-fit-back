089843c3633e333b1d1e1a56419d0cbb10abbcd5
Juan Carlos Perdomo <jcperdomoq@uqvirtual.edu.co>
2019-10-17 10:00:11 -0500

    seed productos

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -28,53 +28,72 @@ class ProductsTableSeeder extends Seeder
         ]);
 
         Product::create([
-            "name"=>"Leche",
-            "description"=>"Es una leche de calidad",
-            "price"=>"2000",
-            "slug" => "leche-calidad"
+            "name"=>"-	YOGURT GRIEGO 1000 Gr. AREQUIPE - COLOUR GREEK",
+            "description"=>"El yogurt griego de Arequipe es un alimento perfecto para consumir con tus frutas, vegetales y cereales preferidos, contiene menos del 3% en carbohidratos y azúcar ademas de 24 gramos de proteína por cada 300 gramos de yogurt, es 95% libre de grasa.
+                            El yogurt griego ayuda a fortalecer el sistema digestivo en general, es rico en bacilos los cuales intervienen en la función de mejorar el sistema inmunológico y al mismo tiempo tienen función reparadora de la flora intestinal, el yogurt griego Color Greek 
+                            es una fuente alta en proteínas ideal para personas de todas las edades que deseen aumentar su masa muscular.",
+            "price"=>"17500",
+            "slug" => "yogurt-griego-1000-gr-arequipe-colour-greek"
         ]);
 
         Product::create([
-            "name"=>"Leche",
-            "description"=>"Es una leche de calidad",
-            "price"=>"2000",
-            "slug" => "leche-calidad"
+            "name"=>"YOGURT GRIEGO 1000 Gr. FRESA SILVESTRE - COLOUR GREEK",
+            "description"=>"El yogurt griego de Fresa silvestre es un alimento perfecto para consumir con tus frutas, vegetales y cereales preferidos, contiene menos del 3% en carbohidratos y azúcar ademas de 24 gramos de proteína por cada 300 gramos de yogurt, es 95% libre de grasa.
+            El yogurt griego ayuda a fortalecer el sistema digestivo en general, es rico en bacilos los cuales intervienen en la función de mejorar el sistema inmunológico y al mismo tiempo tienen función reparadora de la flora intestinal, el yogurt griego Color Greek es una fuente alta en proteínas ideal para personas de todas las edades que deseen aumentar su masa muscular.",
+            "price"=>"17500",
+            "slug" => "yogurt-griego-1000-gr-fresa-silvestre"
         ]);
 
         Product::create([
-            "name"=>"Dulce de leche",
-            "description"=>"El dulce de leche o manjar es un dulce 
-            tradicional latinoamericano, que corresponde 
-            a una variante caramelizada de la leche.",
-            "price"=>"4000",
-            "slug" => "dulce-de-leche"
+            "name"=>"YOGURT GRIEGO 1000 Gr. COOKIES & CREAM - COLOUR GREEK",
+            "description"=>"El yogurt griego de Cookies & Cream es un alimento perfecto para consumir con tus frutas, vegetales y cereales preferidos, contiene menos del 3% en carbohidratos y azúcar ademas de 24 gramos de proteína por cada 300 gramos de yogurt, es 95% libre de grasa.
+                            El yogurt griego ayuda a fortalecer el sistema digestivo en general, es rico en bacilos los cuales intervienen en la función de mejorar el sistema inmunológico y al mismo tiempo tienen función reparadora de la flora intestinal, el yogurt griego Color Greek 
+                            es una fuente alta en proteínas ideal para personas de todas las edades que deseen aumentar su masa muscular.",
+            "price"=>"17500",
+            "slug" => "yogurt-griego-1000-gr-cookies-cream-colour-greek"
         ]);
 
         Product::create([
-            "name"=>"Queso",
-            "description"=>"El queso es un alimento sólido que se obtiene por 
-            maduración de la cuajada de la leche una vez eliminado el suero; sus diferentes variedades dependen del 
-            origen de la leche empleada, de los métodos de elaboración seguidos y del grado de madurez alcanzada",
-            "price"=>"5000",
-            "slug" => "queso"
+            "name"=>"YOGURT GRIEGO 1000 Gr. DURAZNO PERSA - COLOUR GREEK",
+            "description"=>"El yogurt griego de durazno persa es un alimento perfecto para consumir con tus frutas, vegetales y cereales preferidos, contiene menos del 3% en carbohidratos y azúcar ademas de 24 gramos de proteína por cada 300 gramos de yogurt, es 95% libre de grasa.
+            El yogurt griego ayuda a fortalecer el sistema digestivo en general, es rico en bacilos los cuales intervienen en la función de mejorar el sistema inmunológico y al mismo tiempo tienen función reparadora de la flora intestinal, el yogurt griego Color Greek es una fuente 
+            alta en proteínas ideal para personas de todas las edades que deseen aumentar su masa muscular.",
+            "price"=>"17500",
+            "slug" => "yogurt-griego-1000-gr-durazno-persa-colour-greek"
         ]);
 
         Product::create([
-            "name"=>"Queso",
-            "description"=>"El queso es un alimento sólido que se obtiene por 
-            maduración de la cuajada de la leche una vez eliminado el suero; sus diferentes variedades dependen del 
-            origen de la leche empleada, de los métodos de elaboración seguidos y del grado de madurez alcanzada",
-            "price"=>"5000",
-            "slug" => "queso"
+            "name"=>"YOGURT GRIEGO 1000 Gr. VAINILLA - COLOUR GREEK",
+            "description"=>"El yogurt griego de vainilla es un alimento perfecto para consumir con tus frutas, vegetales y cereales preferidos, contiene menos del 3% en carbohidratos y azúcar ademas de 24 gramos de proteína por cada 300 gramos de yogurt, es 95% libre de grasa.
+            El yogurt griego ayuda a fortalecer el sistema digestivo en general, es rico en bacilos los cuales intervienen en la función de mejorar el sistema inmunológico y al mismo tiempo tienen función reparadora de la flora intestinal, el yogurt griego Color Greek es una fuente alta en proteínas ideal para personas de todas las edades que deseen aumentar su masa muscular.",
+            "price"=>"17500",
+            "slug" => "yogurt-griego-1000-gr-vainilla-colur-greek"
         ]);
 
         Product::create([
-            "name"=>"Mora",
-            "description"=>"Mora es el nombre que reciben diversos frutos comestibles de 
-            distintas especies. Las moras son frutas o bayas que, a pesar de proceder de especies 
-            vegetales que son completamente diferentes, poseen aspecto similar y características comunes",
-            "price"=>"3000",
-            "slug" => "mora"
+            "name"=>"YOGURT GRIEGO 150 Gr. DULCE SIN SABOR - COLOUR GREEK",
+            "description"=>"El yogurt griego dulce sin sabor es un alimento perfecto para con-sumir con tus frutas, vegetales y cereales preferidos, con su sabor dulce y natural sentirás cada gota de sabor.
+            El yogurt griego ayuda a fortalecer el sistema digestivo en general, es rico en bacilos los cuales intervienen en la función de mejorar el sistema inmunológico y al mismo tiempo tienen función
+             reparadora de la flora intestinal, el yogurt griego Color Greek es una fuente alta en proteínas ideal para personas de todas las edades que deseen aumentar su masa muscular.",
+            "price"=>"5900",
+            "slug" => "yogurt-griego-150-gr-dulce-sin-sabor"
+        ]);
+
+        Product::create([
+            "name"=>"YOGURT GRIEGO 150 Gr. AREQUIPE - COLOUR GREEK",
+            "description"=>"El yogurt griego de Arequipe es un alimento perfecto para consumir con tus frutas, vegetales y cereales preferidos, contiene menos del 3% en carbohidratos y azúcar ademas de 24 gramos de proteína por cada 300 gramos de yogurt, es 95% libre de grasa.
+            El yogurt griego ayuda a fortalecer el sistema digestivo en general, es rico en bacilos los cuales intervienen en la función de mejorar el sistema inmunológico y al mismo tiempo tienen función reparadora de la flora intestinal, el yogurt griego Color Greek es una fuente
+             alta en proteínas ideal para personas de todas las edades que deseen aumentar su masa muscular.",
+            "price"=>"5900",
+            "slug" => "yogurt-griego-150-gr-arequipe-colour-greek"
+        ]);
+
+        Product::create([
+            "name"=>"-	YOGURT GRIEGO 150 Gr. FRESA SILVESTRE - COLOUR GREEK",
+            "description"=>"El yogurt griego de Fresa silvestre es un alimento perfecto para consumir con tus frutas, vegetales y cereales preferidos, contiene menos del 3% en carbohidratos y azúcar ademas de 24 gramos de proteína por cada 300 gramos de yogurt, es 95% libre de grasa.
+                El yogurt griego ayuda a fortalecer el sistema digestivo en general, es rico en bacilos los cuales intervienen en la función de mejorar el sistema inmunológico y al mismo tiempo tienen función reparadora de la flora intestinal, el yogurt griego Color Greek es una fuente alta en proteínas ideal para personas de todas las edades que deseen aumentar su masa muscular.",
+            "price"=>"5900",
+            "slug" => "yogurt-griego-150-gr-fresa-silvestre-colour-greek"
         ]);
 
     }
