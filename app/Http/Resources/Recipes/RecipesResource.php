a648a33f3808b661d76c2fab7e99d6fd0f04846d
Juan Carlos Perdomo <jcperdomoq@uqvirtual.edu.co>
2019-12-13 12:22:48 -0500

    blog

--- app/Http/Resources/Recipes/RecipesResource.php
+++ app/Http/Resources/Recipes/RecipesResource.php
@@ -29,6 +29,9 @@ class RecipesResource extends JsonResource
             'ingredients'=>$this->ingredients,
             'preparations'=>$this->preparations,
             'supplys'=>$this->supplys,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'deleted_at' => (string) $this->deleted_at ? $this->deleted_at : null,
         ];
     }
 }
